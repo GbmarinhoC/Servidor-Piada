dfcf096279e51a120cb6fc1452115633b685209d
GbmarinhoC <141251772+GbmarinhoC@users.noreply.github.com>
2023-08-22 14:45:04 -0300

    Update KKMultiServer.java

--- KKMultiServer.java
+++ KKMultiServer.java
@@ -5,19 +5,27 @@ import java.io.*;
 
 public class KKMultiServer {
     public static void main(String[] args) throws IOException {
-
-    
-    	//setando nossa porta
         int portNumber = 7777;
+        int maxConnections = 3; 
+        int currentConnections = 0;
         boolean listening = true;
-        //Nao estava rodando pelo terminal porque eu estava dentro do use
-        //para isso fique no src e de use.Nome
-        try (ServerSocket serverSocket = new ServerSocket(portNumber)) { 
+
+        try (ServerSocket serverSocket = new ServerSocket(portNumber)) {
             while (listening) {
-            	// fica nesse while esperando clientes
-	            new KKMultiServerThread(serverSocket.accept()).start();
-	        }
-	    } catch (IOException e) {
+                if (currentConnections < maxConnections) {
+                    Socket clientSocket = serverSocket.accept();
+                    new KKMultiServerThread(clientSocket).start();
+                    currentConnections++;
+                } else {
+                    // Caso tenha atingido o limite, recuse a conexão
+                    Socket clientSocket = serverSocket.accept();
+                    PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                    out.println("Número máximo de conexões atingido. Tente novamente mais tarde.");
+                    out.close();
+                    clientSocket.close();
+                }
+            }
+        } catch (IOException e) {
             System.err.println("Nao foi possivel entrar na porta: " + portNumber);
             System.exit(-1);
         }
